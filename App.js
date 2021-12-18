f6cbe493d2a05c7c1bd70dfbfd3932a4c2e6a2da
Magdiel <mgomez2091@gmail.com>
2021-12-18 01:26:29 -0700

    modified:   App.js 	modified:   package.json 	modified:   yarn.lock

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import Auth from './src/pages/Auth';
 
 export default function App() {
+  const [auth, setAuth] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      {auth ? <Text> Home </Text> :  <Auth />}
+    </PaperProvider>
   );
 }
 
